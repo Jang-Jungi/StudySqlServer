0fdda90544078e8aa3379654bb61380e17f04d40
Jang-Jungi <jjgweb951101@naver.com>
2021-05-14 17:22:31 +0900

    수정

--- 20210513/cte_example.sql
+++ 20210513/cte_example.sql
@@ -10,7 +10,8 @@ AS
 )
 
 SELECT  -- SELECT 심화 내용 언급
-       CASE DIV 
+	   IIF(DIV=0,GRP,'총합계')AS [상품그룹1] -- IIF문을 사용하면 아래 3줄을 1줄로 나타낼 수 있다.
+     , CASE DIV 
 	   WHEN 0 THEN GRP 
 	   WHEN 1 THEN '총합계' END AS [상품그룹]  -- 이런식으로 많이 작업한다
      , SUMM AS [그룹별 구매금액]
